d68d2755cc9ced97583ea75da06979c435530741
tee0zed <tee@zed@gmail.com>
2020-05-05 22:37:14 +0300

    username_downcase callback added

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
+
   belongs_to :user
 
   validates :text, :user, presence: true
   validates :text,  length: { maximum: 255 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ApplicationRecord
+
   has_many :questions
+
+  before_validation :username_downcase
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
 
   validates :username,  length: { maximum: 40 }
+  validates :username, format: { with: /\A[a-zA-Z0-9_]+\z/i }
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, format: { with: /\A[a-zA-Z0-9_]+\z/i }
+
+  private
+
+  def username_downcase
+    self.username = username.downcase
+  end
 end
