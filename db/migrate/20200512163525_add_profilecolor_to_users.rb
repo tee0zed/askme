1ae09278f405841abddbf452a765f8782c8b351d
tee0zed <tee0zed@gmail.com>
2020-05-14 22:42:32 +0300

    default color of UI changed

--- db/migrate/20200512163525_add_profilecolor_to_users.rb
+++ db/migrate/20200512163525_add_profilecolor_to_users.rb
@@ -1,5 +1,5 @@
 class AddProfilecolorToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :profilecolor, :integer, default: 4
+    add_column :users, :profilecolor, :integer, default: 3
   end
 end
